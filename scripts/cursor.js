4bfe6bf3168d3f2ef7930d692749265a222cb537
summertides <fdfgehge@gmail.com>
2023-01-12 12:02:47 -0500

    Small Change
    Cursor now has a dot for better navigation and simulation of a real desktop cursor.

--- scripts/cursor.js
+++ scripts/cursor.js
@@ -1,11 +1,13 @@
 var a = document.querySelectorAll('a');
 const cursorInner = document.getElementById("cursorB")
+const cursorOuter = document.getElementById("cursorA")
 
 const cursorMove = (e)=> {
     const coordinateX = e.clientX;
     const coordinateY = e.clientY;
 
     cursorInner.style.transform = `translate3d(${coordinateX}px, ${coordinateY}px, 0)`;
+    cursorOuter.style.transform = `translate3d(${coordinateX}px, ${coordinateY}px, 0)`;
 }
 
 window.addEventListener('mousemove', cursorMove)
@@ -17,5 +19,15 @@ a.forEach(item => {
 
     item.addEventListener('mouseleave', () => {
         cursorInner.classList.remove('cursorHover');
+    })
+})
+
+a.forEach(item => {
+    item.addEventListener('mousedown', () => {
+        cursorInner.classList.add('cursorDown');
+    })
+
+    item.addEventListener('mouseup', () => {
+        cursorInner.classList.remove('cursorDown')
     })
 })
